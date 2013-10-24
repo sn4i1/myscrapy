c4ee9698079042dcfe0e1c64c01d34a9ea8b1f53
sn4i1 <vargabence@icloud.com>
2013-10-24 13:52:30 +0200

    Update pok_spider.py

--- pok_spider.py
+++ pok_spider.py
@@ -8,18 +8,20 @@ class MySpider(CrawlSpider):
 	name='google'
 	domain_name = 'google'
 	allowed_domains = ['google.hu']
-	start_urls = ['https://www.google.hu/#q=pok']
+	start_urls = ['https://www.google.hu/&q=pok']
 	rules = (Rule (SgmlLinkExtractor(allow=('next\.html', ),restrict_xpaths=('//div[@id="pg"]',)), callback='parse_item', follow= True),)
 	
 	def parse_item(self, response):
 		hxs = HtmlXPathSelector(response)
-		sites= hxs.select()
+		sites= hxs.select('//*[@id="rso"]')
 		items =[]
 		
 		for site in sites:
 			item = PokItem() ### this is the item object I defined in the items file
-			item["title"] = item.select('//*[@id="rso"]/li[1]/div/h3/a/em').extract() ### assuming my item object has “title” field
-			sitem["url"] = item.select('//*[@id="rso"]/li[1]/div/div/div/div[1]/cite').extract()
-			item["desc"] = item.select('//*[@id="rso"]/li[1]/div/div/div/span').extract()			
-			items.append(scraped_item)
+			item["title"] = item.select('/li[1]/div/h3/a/em').extract() ### assuming my item object has “title” field
+			sitem["url"] = item.select('/li[1]/div/div/div/div[1]/cite').extract()
+			item["desc"] = item.select('/li[1]/div/div/div/span').extract()			
+			items.append(item)
 		return(items)
+
+
